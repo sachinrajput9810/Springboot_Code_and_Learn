3d39ec0eb9699d5a269935ca8e331a6adc20082b
sachinrajput9810 <rajputsachin9810@gmail.com>
2025-01-28 20:35:58 +0530

    Annotation based dependency Management

---
+++ Project1_Annotation_Based_Dependency_Mangmt/src/main/java/com/nt/test/App.java
@@ -0,0 +1,19 @@
+package com.nt.test;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.nt.beans.WishMessageGenerator;
+import com.nt.cfgs.appConfig;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+    	AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(appConfig.class);    
+    	WishMessageGenerator generator = ctx.getBean("wmg" , WishMessageGenerator.class) ;
+        String msg = generator.message("Alexa") ;
+        System.out.println(msg);
+        ctx.close();
+    }
+}
