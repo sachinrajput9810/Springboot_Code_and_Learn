66693b011bc83bb427acc8eff4e9ac17238919e1
Sachin Rajput <165654923+sachinrajput9810@users.noreply.github.com>
2025-02-15 13:24:12 +0530

    Delete Proj6_Hospital_Location_layered_Autoconfiguration directory

--- Proj6_Hospital_Location_layered_Autoconfiguration/src/main/java/com/nt/dao/IHospitalDao.java
+++
@@ -1,9 +0,0 @@
-package com.nt.dao;
-
-import java.util.List;
-
-import com.nt.models.Hospital;
-
-public interface IHospitalDao {
-	public List<Hospital> getHospitalByLocation(String location1 , String location2 , String location3) throws Exception ;
-}
--- Proj6_Hospital_Location_layered_Autoconfiguration/src/main/java/com/nt/service/IHospitalService.java
+++
@@ -1,9 +0,0 @@
-package com.nt.service;
-
-import java.util.List;
-
-import com.nt.models.Hospital;
-
-public interface IHospitalService {
-	public List<Hospital> fetchHospitalByLocation(String loc1 , String loc2 , String loc3) throws Exception;
-}
