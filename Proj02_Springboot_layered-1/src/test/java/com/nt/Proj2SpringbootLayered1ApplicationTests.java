31031faaeb0f2682ee0b240367b93adae6680f69
Sachin Rajput <165654923+sachinrajput9810@users.noreply.github.com>
2025-02-15 13:28:32 +0530

    Layered Application 1

---
+++ Proj02_Springboot_layered-1/src/test/java/com/nt/Proj2SpringbootLayered1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proj2SpringbootLayered1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
